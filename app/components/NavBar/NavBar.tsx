6b55c395c6f2ed3fd258c8d87bf22435a2637bd0
scotty974 <fabienetheve@hotmail.com>
2024-05-27 13:08:22 +0200

    add navbar

---
+++ app/components/NavBar/NavBar.tsx
@@ -0,0 +1,32 @@
+import { Linkedin,Mail, Github } from "lucide-react";
+function NavBar() {
+  return (
+    <header className=" fixed left-0 top-0 z-10">
+      <nav className="flex items-start flex-col justify-between w-40 h-screen p-4">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-3xl font-bold">Etheve Fabien</h1>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <a href="#" className="hover:underline">A propos</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">Projets</a>
+            </li>
+            <li>
+              <a href="#" className="hover:underline">Contacts</a>
+            </li>
+          </ul>
+          <div className="flex flex-wrap flex-col gap-2">
+            <a href="https://www.linkedin.com/in/fabien-etheve-7125a0226/" target="_blank"><Linkedin/></a>
+            <a href="mailto:fabienethevepro@gmail.com"><Mail/></a>
+            <a href="https://github.com/scotty974" target="_blank"><Github/></a>
+          </div>
+        </div>
+
+        <p className="text-sm">© 2022 Etheve Fabien. Tous droits réservés.</p>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,33 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Environment, Text} from "@react-three/drei";
+import { Environment, Text } from "@react-three/drei";
 import Sphere from "./components/sphere/Sphere";
 import { Physics } from "@react-three/cannon";
 import Pointer from "./components/pointer/Pointer";
+import NavBar from "./components/NavBar/NavBar";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
-      <section className="w-full h-screen">
-        <Canvas shadows gl={{ antialias: false }} dpr={[1, 1.5]} camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}>
+      <section className="w-full h-screen flex justify-center ">
+        <NavBar />
+        <Canvas
+          shadows
+          gl={{ antialias: false }}
+          dpr={[1, 1.5]}
+          camera={{ position: [0, 0, 20], fov: 35, near: 1, far: 40 }}
+        >
           <ambientLight intensity={0.5} />
-          <OrbitControls />
           <Environment preset="sunset" />
           <Physics>
             <Pointer />
             <Sphere />
           </Physics>
-          <Text fontSize={1.5} color={"black"}>Fabien ETHEVE</Text>
+          <Text fontSize={1.5} color={"black"} fontWeight={"bold"}>
+            Fabien ETHEVE
+          </Text>
         </Canvas>
       </section>
+      <section className="h-screen"></section>
     </main>
   );
 }
