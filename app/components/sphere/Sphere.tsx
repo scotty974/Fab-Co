5b78211a5d9ccd29c19a55b1d9628056e7494d13
scotty974 <fabienetheve@hotmail.com>
2024-05-27 12:34:23 +0200

    add gravity sphere

--- app/components/sphere/Sphere.tsx
+++ app/components/sphere/Sphere.tsx
@@ -1,16 +1,45 @@
 // components/sphere/Sphere.js
-import { useRef } from "react";
+import { useSphere } from "@react-three/cannon";
 import { useFrame } from "@react-three/fiber";
-import { Mesh } from "three";
-const Sphere = ({position} : {position: any}) => {
-  let myRef = useRef<Mesh>(null);
-
+import * as THREE from "three";
+import { Outlines } from "@react-three/drei";
+const rfs = THREE.MathUtils.randFloatSpread;
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
+const baubleMaterial = new THREE.MeshStandardMaterial({ color: "white", roughness: 0, envMapIntensity: 1 })
+const Sphere = ({
+  mat = new THREE.Matrix4(),
+  vec = new THREE.Vector3(),
+  ...props
+}) => {
+  const [ref, api] = useSphere(() => ({
+    args: [1],
+    mass: 1,
+    angularDamping: 0.1,
+    linearDamping: 0.65,
+    position: [rfs(20), rfs(20), rfs(20)],
+  }));
+  useFrame((state) => {
+    for (let i = 0; i < 40; i++) {
+      if (ref.current) {
+        ref.current.getMatrixAt(i, mat);
+      }
+      api
+        .at(i)
+        .applyForce(
+          vec
+            .setFromMatrixPosition(mat)
+            .normalize()
+            .multiplyScalar(-40)
+            .toArray(),
+          [0, 0, 0]
+        );
+    }
+  });
   return (
-    <mesh ref={myRef} position={position}>
-      <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color="orange" />
-    </mesh>
-  );
+    <instancedMesh ref={ref} castShadow receiveShadow args={[sphereGeometry, baubleMaterial, 40]}>
+      <Outlines thickness={0} />
+    </instancedMesh>
+  )
 };
 
 export default Sphere;
