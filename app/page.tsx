e05d92c227e4e9e3230e39dc624fc6d4d82f2375
scotty974 <fabienetheve@hotmail.com>
2024-05-27 11:42:27 +0200

    add scene react three fiber

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-
-
+"use client";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
-      <section>
-        
+      <section className="w-full h-screen">
+        <Canvas>
+          <ambientLight intensity={0.5} />
+          <OrbitControls />
+        </Canvas>
       </section>
     </main>
   );
